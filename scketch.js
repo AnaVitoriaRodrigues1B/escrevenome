ad0f3c32e22b3e2b248c53864e53a77e25fb0777
AnaVitoriaRodrigues1B <00001121084928sp@al.educacao.sp.gov.br>
2024-08-07 15:55:15 -0300

    adicionando arquivos js e css.

---
+++ scketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(600, 600);
+    background("purple");
+  }
+  
+  function draw() {
+    stroke("#FF9800");
+    fill("rgb(47,255,0)");
+  
+  if(mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35)
+    
+  }
+    
+     }
